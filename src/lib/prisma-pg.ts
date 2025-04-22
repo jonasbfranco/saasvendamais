82cd3db97073d5f4fb062ffd71209afe848a953a
Jonas Baptista Franco <jonas@MacBook-Air-de-Jonas.local>
2025-04-21 21:28:52 -0300

    21/04/2025 - MVP - Vercel Errors - DeepSeek-1.4.3

--- src/lib/prisma-pg.ts
+++ src/lib/prisma-pg.ts
@@ -1,13 +1,21 @@
-import { Pool } from "@neondatabase/serverless";
+/* import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-/**
+
+ */
+
+
+/** COMENTARIO
  * Gera uma instância do banco de dados com o adaptador serverless Neon
  */
+
+
+
+/* 
 export function PrismaGetInstance(): PrismaClient {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   const adapter = new PrismaNeon(pool);
   const prisma = new PrismaClient({ adapter });
 
   return prisma;
-}
+} */
